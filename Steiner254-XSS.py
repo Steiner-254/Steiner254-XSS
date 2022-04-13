fb15cc997566a17358fe1a09973f3a93289f236d
Steiner254 <70921705+Steiner-254@users.noreply.github.com>
2022-04-13 00:09:51 -0700

    Update Steiner254-XSS.py

--- Steiner254-XSS.py
+++ Steiner254-XSS.py
@@ -1,27 +1,25 @@
-from traceback import print_tb
 import requests
 import threading
-import sys
 from colorama import Fore, Back, Style
 
 
 print(
-        Fore.YELLOW +
+        Fore.BLUE +
          """
-          ____  _       _                ____  ____  _  _   
+ ____  _       _                ____  ____  _  _   
 / ___|| |_ ___(_)_ __   ___ _ _|___ \| ___|| || |  
 \___ \| __/ _ \ | '_ \ / _ \ '__|__) |___ \| || |_ 
  ___) | ||  __/ | | | |  __/ |  / __/ ___) |__   _|
 |____/ \__\___|_|_| |_|\___|_| |_____|____/   |_| 
-
-         
+     
                                           [ twitter.com/steiner254 ]
                                           [ github.com/Steiner-254 ]
         """ + Fore.RESET)
 
 print()
 print()
-
+file = open('url.txt','r')
+payloads = open('payloads.txt','r')
 def Send_req(url,payload):
     #while url[-1] != '=':
      #   url = url[:-1]
@@ -36,21 +34,9 @@ def Send_req(url,payload):
 
     except Exception as e:
         pass
-
-if __name__ == "__main__":
-    try:
-        payloads = sys.argv[1].strip()
-        urls = sys.argv[2].strip()
-    except IndexError:
-        print("[-] Usage: python3 %s <payloads> <urls>" % sys.argv[0])
-        print("[-] Example: python3 %s payload.txt url.txt" % sys.argv[0])
-        sys.exit(-1)
-
-payloads = open(f'{payloads}', 'r', encoding='utf-8')
-urls = open(f'{urls}', 'r', encoding='utf-8')
-payloads = payloads.readlines()
+file = file.readlines()
 for payload in payloads:
-    for url in urls:
+    for url in file:
         url = url.strip('\n')
         payload = payload.strip('\n')
         threading.Thread(target=Send_req,args=(url,payload,)).start()
