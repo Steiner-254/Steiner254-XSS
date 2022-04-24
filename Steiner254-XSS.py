de6cb701e6e331bf1d02de1927659901dfc3069c
Steiner254 <70921705+Steiner-254@users.noreply.github.com>
2022-04-24 00:38:54 -0700

    Update Steiner254-XSS.py

--- Steiner254-XSS.py
+++ Steiner254-XSS.py
@@ -31,7 +31,7 @@ def Send_req(url,payload):
 
         res = requests.get(url)
         if payload in res.text:
-           print(Fore.RED +'XSS Found   -->','   ' , f"{url}" + Fore.RESET)
+           print(Fore.GREEN +'XSS Found   -->','   ' , f"{url}" + Fore.RESET)
 
 
     except Exception as e:
